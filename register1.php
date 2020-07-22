e144d76d64080370b3c5387244f9c00ebb2d4ab4
Katinan <64100958+katinanleon@users.noreply.github.com>
2020-07-22 12:52:49 +0000

    Delete register1.php

--- register1.php
+++
@@ -1,8 +0,0 @@
-<?php
- include 'conn.php';
-
- $sql = "INSERT INTO table_student (email, name, password) VALUES ('".$_POST["email"]."','".$_POST["name"]."','".$_POST["password"]."')";
- $result = $conn->query($sql);
- 
- 
- $conn->close();
